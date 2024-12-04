c9db7a1ff3db3041b92b846b03ebdf692a98c9b0
William Dahlberg <william.dahlberg@itxpt.org>
2024-12-04 15:12:28 +0100

    prettier solutions

--- 4.py
+++ 4.py
@@ -4,71 +4,32 @@ with open("4.input") as f:
 size = x, y = len(lines), len(lines[0])
 
 
+def get(c, i, j):
+    if i < 0 or j < 0:
+        return ""
+    try:
+        return c[i][j]
+    except IndexError:
+        return ""
+
+
 def xmas():
     xmases = 0
     for i in range(x):
         for j in range(y):
-            if lines[i][j] == "X":
-                # forward
-                if (
-                    j + 3 < y
-                    and lines[i][j + 1] + lines[i][j + 2] + lines[i][j + 3] == "MAS"
-                ):
-                    xmases += 1
-                # backward
-                if (
-                    j - 3 >= 0
-                    and lines[i][j - 1] + lines[i][j - 2] + lines[i][j - 3] == "MAS"
-                ):
-                    xmases += 1
-                # down
-                if (
-                    i + 3 < x
-                    and lines[i + 1][j] + lines[i + 2][j] + lines[i + 3][j] == "MAS"
-                ):
-                    xmases += 1
-                # up
-                if (
-                    i - 3 >= 0
-                    and lines[i - 1][j] + lines[i - 2][j] + lines[i - 3][j] == "MAS"
-                ):
-                    xmases += 1
+            if get(lines, i, j) == "X":
+                words = [
+                    get(lines, i, j + 1) + get(lines, i, j + 2) + get(lines, i, j + 3),
+                    get(lines, i, j - 1) + get(lines, i, j - 2) + get(lines, i, j - 3),
+                    get(lines, i + 1, j) + get(lines, i + 2, j) + get(lines, i + 3, j),
+                    get(lines, i - 1, j) + get(lines, i - 2, j) + get(lines, i - 3, j),
+                    get(lines, i + 1, j + 1) + get(lines, i + 2, j + 2) + get(lines, i + 3, j + 3),
+                    get(lines, i + 1, j - 1) + get(lines, i + 2, j - 2) + get(lines, i + 3, j - 3),
+                    get(lines, i - 1, j + 1) + get(lines, i - 2, j + 2) + get(lines, i - 3, j + 3),
+                    get(lines, i - 1, j - 1) + get(lines, i - 2, j - 2) + get(lines, i - 3, j - 3),
+                ]
 
-                # diagonal down right
-                if (
-                    j + 3 < y
-                    and i + 3 < x
-                    and lines[i + 1][j + 1] + lines[i + 2][j + 2] + lines[i + 3][j + 3]
-                    == "MAS"
-                ):
-                    xmases += 1
-
-                # diagonal down left
-                if (
-                    j - 3 >= 0
-                    and i + 3 < x
-                    and lines[i + 1][j - 1] + lines[i + 2][j - 2] + lines[i + 3][j - 3]
-                    == "MAS"
-                ):
-                    xmases += 1
-
-                # diagonal up right
-                if (
-                    j + 3 < y
-                    and i - 3 >= 0
-                    and lines[i - 1][j + 1] + lines[i - 2][j + 2] + lines[i - 3][j + 3]
-                    == "MAS"
-                ):
-                    xmases += 1
-
-                # diagonal up left
-                if (
-                    j - 3 >= 0
-                    and i - 3 >= 0
-                    and lines[i - 1][j - 1] + lines[i - 2][j - 2] + lines[i - 3][j - 3]
-                    == "MAS"
-                ):
-                    xmases += 1
+                xmases += words.count("MAS")
 
     return xmases
 
@@ -79,19 +40,14 @@ def x_mas():
     for i in range(1, x - 1):
         for j in range(1, y - 1):
             if lines[i][j] == "A":
-                mases = 0
-                if lines[i - 1][j - 1] == "M" and lines[i + 1][j + 1] == "S":
-                    mases += 1
-                if lines[i - 1][j - 1] == "S" and lines[i + 1][j + 1] == "M":
-                    mases += 1
-                if lines[i - 1][j + 1] == "M" and lines[i + 1][j - 1] == "S":
-                    mases += 1
-                if lines[i - 1][j + 1] == "S" and lines[i + 1][j - 1] == "M":
-                    mases += 1
-
-                if mases == 2:
+                diag_1 = lines[i - 1][j - 1] + lines[i + 1][j + 1]
+                diag_2 = lines[i - 1][j + 1] + lines[i + 1][j - 1]
+                if (diag_1 == "MS" or diag_1 == "SM") and (
+                    diag_2 == "MS" or diag_2 == "SM"
+                ):
                     x_mases += 1
+
     return x_mases
 
 
-print(x_mas())
+print(xmas())
