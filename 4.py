c1b674d5157b13f6f560f1b17fb3965fa9f03ebb
William Dahlberg <william.dahlberg@itxpt.org>
2024-12-04 16:43:16 +0100

    itertools!

--- 4.py
+++ 4.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 with open("4.input") as f:
     lines = [l.strip() for l in f.readlines()]
 
@@ -15,21 +18,20 @@ def get(i, j):
 
 def xmas():
     xmases = 0
-    for i in range(x):
-        for j in range(y):
-            if get(i, j) == "X":
-                words = [
-                    get(i, j + 1) + get(i, j + 2) + get(i, j + 3),
-                    get(i, j - 1) + get(i, j - 2) + get(i, j - 3),
-                    get(i + 1, j) + get(i + 2, j) + get(i + 3, j),
-                    get(i - 1, j) + get(i - 2, j) + get(i - 3, j),
-                    get(i + 1, j + 1) + get(i + 2, j + 2) + get(i + 3, j + 3),
-                    get(i + 1, j - 1) + get(i + 2, j - 2) + get(i + 3, j - 3),
-                    get(i - 1, j + 1) + get(i - 2, j + 2) + get(i - 3, j + 3),
-                    get(i - 1, j - 1) + get(i - 2, j - 2) + get(i - 3, j - 3),
-                ]
+    for i, j in itertools.product(range(x), range(y)):
+        if get(i, j) == "X":
+            words = [
+                get(i, j + 1) + get(i, j + 2) + get(i, j + 3),
+                get(i, j - 1) + get(i, j - 2) + get(i, j - 3),
+                get(i + 1, j) + get(i + 2, j) + get(i + 3, j),
+                get(i - 1, j) + get(i - 2, j) + get(i - 3, j),
+                get(i + 1, j + 1) + get(i + 2, j + 2) + get(i + 3, j + 3),
+                get(i + 1, j - 1) + get(i + 2, j - 2) + get(i + 3, j - 3),
+                get(i - 1, j + 1) + get(i - 2, j + 2) + get(i - 3, j + 3),
+                get(i - 1, j - 1) + get(i - 2, j - 2) + get(i - 3, j - 3),
+            ]
 
-                xmases += words.count("MAS")
+            xmases += words.count("MAS")
 
     return xmases
 
@@ -37,13 +39,12 @@ def xmas():
 # part 2
 def x_mas():
     x_mases = 0
-    for i in range(1, x - 1):
-        for j in range(1, y - 1):
-            if lines[i][j] == "A":
-                diag_1 = lines[i - 1][j - 1] + lines[i + 1][j + 1]
-                diag_2 = lines[i - 1][j + 1] + lines[i + 1][j - 1]
-                if {diag_1, diag_2} <= {"MS", "SM"}:
-                    x_mases += 1
+    for i, j in itertools.product(range(1, x - 1), range(1, y - 1)):
+        if lines[i][j] == "A":
+            diag_1 = lines[i - 1][j - 1] + lines[i + 1][j + 1]
+            diag_2 = lines[i - 1][j + 1] + lines[i + 1][j - 1]
+            if {diag_1, diag_2} <= {"MS", "SM"}:
+                x_mases += 1
 
     return x_mases
 
