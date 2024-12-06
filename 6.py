87af446b738470c6d609c80de35c3c179274a241
William Dahlberg <william.dahlberg@itxpt.org>
2024-12-06 13:53:17 +0100

    part 2 almost working

--- 6.py
+++ 6.py
@@ -1,11 +1,12 @@
 from copy import copy
+import itertools
 
 
 with open("6.input") as f:
-    grid = [l.strip() for l in f.readlines()]
+    lines = [l.strip() for l in f.readlines()]
 
 
-def find_start():
+def find_start(grid):
     for i, line in enumerate(grid):
         for j, char in enumerate(line):
             match char:
@@ -28,7 +29,7 @@ def turn_right(direction):
     }[direction]
 
 
-def get_char(pos):
+def get_char(pos, grid):
     return grid[pos[1]][pos[0]]
 
 
@@ -36,12 +37,12 @@ def get_next(pos, direction):
     return tuple(map(sum, zip(pos, direction)))
 
 
-def walk(pos, direction):
+def walk(pos, direction, grid):
     new_pos = get_next(pos, direction)
-    if get_char(new_pos) == "#":
+    if get_char(new_pos, grid) == "#":
         direction = turn_right(direction)
     new_pos = get_next(pos, direction)
-    if get_char(new_pos) == "#":
+    if get_char(new_pos, grid) == "#":
         direction = turn_right(direction)
     new_pos = get_next(pos, direction)
 
@@ -52,13 +53,13 @@ def walk(pos, direction):
 
 
 def find_cells():
-    position, direction = find_start()
+    position, direction = find_start(lines)
     print(f"start: {position=}, {direction=}")
     found = set()
     found.add(position)
     while True:
         try:
-            position, direction = walk(position, direction)
+            position, direction = walk(position, direction, lines)
         except IndexError:
             return found
 
@@ -69,4 +70,43 @@ def calc():
     print(len(find_cells()))
 
 
-print(calc())
+# part 2
+def get_new_grid(pos, char):
+    new_grid = copy(lines)
+    old_line = new_grid[pos[1]]
+    new_line = old_line[: pos[0]] + char + old_line[pos[0] + 1 :]
+    new_grid[pos[1]] = new_line
+    return new_grid
+
+
+def find_working_positions():
+    working_positions = set()
+    for x, y in find_cells():
+        pos = (x, y)
+        if (x, y) == (3, 6):
+            pass
+        if get_char(pos, lines) != ".":
+            continue
+        new_grid = get_new_grid(pos, "#")
+        if try_position(new_grid):
+            working_positions.add(pos)
+    return working_positions
+
+
+def try_position(grid):
+    vector = find_start(grid)
+    found = set()
+    found.add(vector)
+    while True:
+        try:
+            vector = walk(*vector, grid)
+        except IndexError:
+            return False
+
+        if vector in found:
+            return True
+        found.add(vector)
+
+
+calc()
+print(len(find_working_positions()))
