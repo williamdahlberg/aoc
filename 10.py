a46efc6e35c296dd5ce2acf48fdcc11c5b98edca
William Dahlberg <william.dahlberg@itxpt.org>
2024-12-10 12:43:05 +0100

    last fix

--- 10.py
+++ 10.py
@@ -7,11 +7,11 @@ grid = {
 
 def path(zero_c, c):
     for dir in (1, -1, 1j, -1j):
-        new_n, new_s = grid.get(c + dir, (0, {}))
+        new_n, new_s = grid.get(c + dir, (0, []))
         if new_n == grid[c][0] + 1:
             new_s.append(zero_c)
             path(zero_c, c + dir)
 
 
 [path(c, c) for c in grid if grid[c][0] == 0]
-print(list(map(sum, zip(*[(len(set(s)), len(s)) for n, s in grid.values() if n > 8]))))
+print(list(map(sum, zip(*[(len(set(s)), len(s)) for n, s in grid.values() if n == 9]))))
