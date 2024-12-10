98b46c498da7b1f34e1792c245ee2f82836ba336
William Dahlberg <william.dahlberg@itxpt.org>
2024-12-10 11:55:15 +0100

    no visited p1

--- 10.py
+++ 10.py
@@ -5,17 +5,13 @@ grid = {
 }
 
 
-def path(zero_c, c, visited=None):
-    visited = visited.add(c) if visited else set()
+def path(zero_c, c):
     for dir in (1, -1, 1j, -1j):
         new_n, new_s = grid.get(c + dir, (0, {}))
         if new_n == grid[c][0] + 1:
             new_s.add(zero_c)
-            path(zero_c, c + dir, visited)
+            path(zero_c, c + dir)
 
 
-for c in grid:
-    if grid[c][0] == 0:
-        path(c, c)
-
+[path(c, c) for c in grid if grid[c][0] == 0]
 print(sum([len(s) for _, (n, s) in grid.items() if n == 9]))
