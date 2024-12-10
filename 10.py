93245c9ff199be9b0dc5ff7dc276fd42f6e886f0
William Dahlberg <william.dahlberg@itxpt.org>
2024-12-10 12:03:35 +0100

    p2

--- 10.py
+++ 10.py
@@ -1,5 +1,5 @@
 grid = {
-    x + y * 1j: (int(n), set())
+    x + y * 1j: (int(n), list())
     for y, r in enumerate(open("10.input").readlines())
     for x, n in enumerate(r.strip())
 }
@@ -9,7 +9,7 @@ def path(zero_c, c):
     for dir in (1, -1, 1j, -1j):
         new_n, new_s = grid.get(c + dir, (0, {}))
         if new_n == grid[c][0] + 1:
-            new_s.add(zero_c)
+            new_s.append(zero_c)
             path(zero_c, c + dir)
 
 
